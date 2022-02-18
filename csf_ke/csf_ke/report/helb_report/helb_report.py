31e5314c10a6ce9025a1d10f54a3654bb195bed1
navariltd <nkubitudouglas@gmail.com>
2022-02-18 15:24:15 +0300

    feat: query builder

--- csf_ke/csf_ke/report/helb_report/helb_report.py
+++ csf_ke/csf_ke/report/helb_report/helb_report.py
@@ -2,79 +2,89 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe, erpnext
+import frappe
 from frappe import _
+from pypika import functions as fn
 
 def execute(filters=None):
+	return HelbReport(filters).run()
 
-	company_currency = erpnext.get_company_currency(filters.get("company"))
-	columns = get_columns()
-	data = get_data(filters,company_currency)
+class HelbReport(object):
+	def __init__(self, filters=None):
+		self.filters = frappe._dict(filters or {})
 
-	return columns, data
+		if not self.filters.to_date:
+			self.filters.to_date = "ss.posting_date <= %(to_date)s"
 
-def get_columns():
-	columns = [			
-		{
-		'label': _('Employee ID'),
-		'fieldname': 'employee',
-		'options': 'Employee',
-		'width': 180
-		},
-		{
-		'label': _('Employee Names'),
-		'fieldname': 'employee_name',
-		'fieldtype': 'Read Only',
-		'width': 260
-		},
-		{
-		'label': _('National ID'),
-		'fieldname': 'national_id',
-		'fieldtype': 'Data',
-		'width': 180
-		},
-		{
-		'label': _('Amount'),
-		'fieldname': 'amount',
-		'fieldtype': 'Currency',		
-		'width': 200
-		}
-	]
+		if not self.filters.from_date:
+			self.filters.from_date = "ss.posting_date >= %(from_date)s"
+
+		if not self.filters.company:
+			self.filters.company = "ss.company = %(company)s"
+
+		if not self.filters.salary_component:
+			self.filters.salary_component = "sd.salary_component = %(salary_component)s"
 		
-	return columns
+		if not self.filters.currency:
+			self.filters.currency =  "ss.currency = %(currency)s"
 
-def get_data(filters,company_currency,conditions=""):
-	conditions = get_conditions(filters,company_currency)
+		self.query_filters = {'posting_date': ['between', [self.filters.from_date, self.filters.to_date]]}
 
-	if filters.from_date > filters.to_date:
-		frappe.throw(_("To Date cannot be before From Date. {}").format(filters.to_date))
+	def run(self):
+		columns = self.get_columns()
+		data = self.get_data()
 
-	data = frappe.db.sql("""
-	SELECT  ss.employee, ss.employee_name,
-	        ss.start_date, ss.end_date, 
-			e.national_id, sd.amount
-	FROM `tabEmployee` e, `tabSalary Slip` ss, `tabSalary Detail` sd
-	WHERE %s
-		and e.name = ss.employee
-		and sd.parent = ss.name		
-		and sd.amount != 0	
-	""" % conditions, filters, as_dict=1)
+		return columns, data
 
-	return data
+	def get_columns(self):
+		return [			
+			{
+			'label': _('Employee ID'),
+			'fieldname': 'employee',
+			'options': 'Employee',
+			'width': 180
+			},
+			{
+			'label': _('Employee Names'),
+			'fieldname': 'employee_name',
+			'fieldtype': 'Read Only',
+			'width': 260
+			},
+			{
+			'label': _('National ID'),
+			'fieldname': 'national_id',
+			'fieldtype': 'Data',
+			'width': 180
+			},
+			{
+			'label': _('Amount'),
+			'fieldname': 'amount',
+			'fieldtype': 'Currency',		
+			'width': 200
+			}
+		]
+			
+	def get_data(self):
+			if self.filters.from_date > self.filters.to_date:
+				frappe.throw(_("To Date cannot be before From Date. {}").format(self.filters.to_date))
 
-def get_conditions(filters,company_currency):
-	conditions = ""
-	doc_status = {"Draft": 0, "Submitted": 1, "Cancelled": 2}
+			e = frappe.qb.DocType('Employee')
+			ss = frappe.qb.DocType('Salary Slip')
+			sd = frappe.qb.DocType('Salary Detail')
 
-	if filters.get("docstatus"):
-		conditions += "ss.docstatus = {0}".format(doc_status[filters.get("docstatus")])
-
-	if filters.get("from_date"): conditions += " and ss.start_date >= %(from_date)s"
-	if filters.get("to_date"): conditions += " and ss.end_date <= %(to_date)s"
-	if filters.get("company"): conditions += " and ss.company = %(company)s"
-	if filters.get("salary_component"): conditions += " and sd.salary_component = %(salary_component)s"
-	if filters.get("currency") and filters.get("currency") != company_currency:
-		conditions += " and ss.currency = %(currency)s"
-
-	
-	return conditions
+			return(
+				frappe.qb.from_(e)
+				.inner_join(ss)		
+				.on(e.name == ss.employee)
+				.left_join(sd)
+				.on(sd.parent == ss.name)
+				.select( ss.employee, ss.employee_name, ss.posting_date, 
+					e.national_id, sd.amount, sd.salary_component )
+				.where(ss.company == self.filters.company)
+				.where(fn.Coalesce(ss.posting_date)[self.filters.from_date:self.filters.to_date])
+				.where(sd.salary_component == self.filters.salary_component)
+				.where(ss.currency == self.filters.currency)
+				.where(ss.docstatus == 1 )
+				.where(sd.amount != 0)			
+				.orderby(ss.posting_date)
+			).run(as_dict=True)	
