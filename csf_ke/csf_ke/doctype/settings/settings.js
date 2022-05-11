52a9c8d2ce4bf49dba9c5a01c052de8d63acbcc8
navariltd <nkubitudouglas@gmail.com>
2022-05-11 17:38:28 +0300

    update: doctype

---
+++ csf_ke/csf_ke/doctype/settings/settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Navari Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
