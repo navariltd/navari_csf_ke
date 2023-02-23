2e250615f7ef70dd6e4dbefedf73d7f465d33798
Lucky-Tsuma <tsumahmumba@gmail.com>
2023-02-23 17:24:00 +0300

    feat: rewrite kenya sales report to match new format
    Besides fiters added in js file (`Is Return` and `Tax Template`), report has new columns defined:
    1. Customer Pin (pin_of_purchaser)
    2. ETR Serial Number
    3. ETR Invoice Number
    4. Description of Goods/Services
    5. Taxable Value
    6. Amount of VAT

--- csf_ke/csf_ke/report/kenya_sales_tax_report/kenya_sales_tax_report.py
+++ csf_ke/csf_ke/report/kenya_sales_tax_report/kenya_sales_tax_report.py
@@ -13,17 +13,6 @@ class KenyaSalesTaxReport(object):
 	def __init__(self, filters=None):
 		self.filters = frappe._dict(filters or {})
 
-		if not self.filters.to_date:
-			self.filters.to_date = "invoice.posting_date <= %(to_date)s"
-
-		if not self.filters.from_date:
-			self.filters.from_date = "invoice.posting_date >= %(from_date)s"
-
-		if not self.filters.company:
-			self.filters.company = "invoice.company = %(company)s"
-
-		self.query_filters = {'posting_date': ['between', [self.filters.from_date, self.filters.to_date]]}
-
 	def run(self):
 		columns = self.get_columns()
 		data = self.get_data()
@@ -31,65 +20,194 @@ class KenyaSalesTaxReport(object):
 		return columns, data
  
 	def get_columns(self):
-		return [
+		columns =  [
 				{
-					"label": _("Customer"),
-					"fieldname": "customer",
-					"fieldtype": "Link",
-					"options": "Customer",
-					"width": 250
+					"label": _("PIN of purchaser"),
+					"fieldname": "pin_of_purchaser",
+					"fieldtype": "Data",
+					"width": 160
 				},
 				{
-					"label":_("Date"),
-					"fieldname": "posting_date",
+					"label": _("Name of purchaser"),
+					"fieldname": "name_of_purchaser",
+					"fieldtype": "Data",
+					"width": 240
+				},
+				{
+					"label": _("ETR Serial Number"),
+					"fieldname": "etr_serial_number",
+					"fieldtype": "Data",
+					"width": 200
+				},
+				{
+					"label": _("ETR Invoice Number"),
+					"fieldname": "etr_invoice_number",
+					"fieldtype": "Data",
+					"width": 200
+				},
+				{
+					"label":_("Invoice Date"),
+					"fieldname": "invoice_date",
 					"fieldtype": "Date",
-					"width": 150
+					"width": 160
 				},
 				{
 					"label": _("Invoice Number"),
-					"fieldname": "name",
+					"fieldname": "invoice_name",
 					"fieldtype": "Link",
 					"options": "Sales Invoice",
-					"width": 230
+					"width": 200
 				},
 				{
-					"label": _("Amount"),
-					"fieldname": "grand_total",
+					"label": _("Description of Goods/Services"),
+					"fieldname": "description_of_goods_services",
+					"fieldtype": "Data",
+					"width": 280
+				},
+				{
+					"label": _("Taxable Value(Ksh)"),
+					"fieldname": "taxable_value",
 					"fieldtype": "Currency",
-					"width": 150
+					"width": 160
 				},
 				{
-					"label": _("Total Taxes"),
-					"fieldname": "amount",
+					"label": _("Amount of VAT(Ksh)"),
+					"fieldname": "amount_of_vat",
 					"fieldtype": "Currency",
-					"width": 150
-				},
+					"width": 160
+				}
+		]
+
+		if self.filters.is_return == "Is Return":
+			columns += [
 				{
-					"label": _("Tax Template"),
-					"fieldname": "tax",
+					"label": _("Return Against"),
+					"fieldname": "return_against",
 					"fieldtype": "Link",
-					"options": "Item Tax Template",
-					"width": 250
+					"options": "Sales Invoice",
+					"width": 200
 				}
 			]
+
+		# columns += [
+		# 	{
+		# 		"label":_("Relevant Invoice Number"),
+		# 		"fieldname": "relevant_invoice_number",
+		# 		"fieldtype": "Data",
+		# 		"width": 200
+		# 	},
+		# 	{
+		# 		"label":_("Relevant Invoice Date"),
+		# 		"fieldname": "relevant_invoice_date",
+		# 		"fieldtype": "Date",
+		# 		"width": 200
+		# 	}
+		# ]
+
+		return columns
+	
 
 	def get_data(self):
 		if self.filters.from_date > self.filters.to_date:
 			frappe.throw(_("To Date cannot be before From Date. {}").format(self.filters.to_date))
 
-		invoice = frappe.qb.DocType('Sales Invoice')
-		taxesandcharges = frappe.qb.DocType('Sales Taxes and Charges')
+		company, from_date, to_date, is_return, tax_template = self.filters.company, self.filters.from_date, self.filters.to_date, self.filters.is_return, self.filters.tax_template
 
-		return (
-			frappe.qb.from_(invoice)
-			.left_join(taxesandcharges)
-			.on(invoice.name == taxesandcharges.parent)
-			.select(invoice.customer, invoice.posting_date, invoice.name, invoice.grand_total, invoice.company,
-				fn.IfNull(taxesandcharges.tax_amount,0).as_('amount'),
-				fn.Concat(taxesandcharges.account_head,' - ', taxesandcharges.rate).as_('tax'))
-			.where(invoice.docstatus == 1)
-			.where(fn.Coalesce(invoice.posting_date)[self.filters.from_date:self.filters.to_date])
-			.where(invoice.company == self.filters.company)
-			.groupby(invoice.customer,invoice.posting_date,invoice.name,taxesandcharges.account_head)
-			.orderby(invoice.customer,invoice.posting_date)
-		).run(as_dict=True)
+		conditions = " AND 1=1 "
+
+		if(company):
+			conditions += f" AND sales_invoice.company = '{company}'"
+		if(is_return == "Is Return"):
+			conditions += f" AND sales_invoice.is_return = 1"
+		if(is_return == "Normal Sales Invoice"):
+			conditions += f" AND sales_invoice.is_return = 0"
+
+		report_details = []
+
+		sales_invoices = frappe.db.sql(f"""
+			SELECT
+				IFNULL(customer.tax_id, NULL) as pin_of_purchaser,
+				sales_invoice.customer_name as name_of_purchaser,
+				sales_invoice.etr_serial_number as etr_serial_number,
+				sales_invoice.etr_invoice_number as etr_invoice_number,
+				sales_invoice.posting_date as invoice_date,
+				sales_invoice.name as invoice_name,
+				sales_invoice.total as invoice_total_sales,
+				sales_invoice.return_against as return_against
+			FROM 
+    			`tabSales Invoice` as `sales_invoice`
+			INNER JOIN 
+    			`tabCustomer` as `customer` 
+			ON
+    			customer.name = sales_invoice.customer
+			WHERE (sales_invoice.posting_date BETWEEN '{from_date}' AND '{to_date}') {conditions};
+		""", as_dict = 1)
+
+		for sales_invoice in sales_invoices:
+			report_details.append(sales_invoice)
+
+			condition_tax_template = " AND 1=1 "
+
+			if tax_template:
+				condition_tax_template += f"AND sales_invoice_item.item_tax_template = '{tax_template}'"
+
+			items_services = frappe.db.sql(f"""
+				SELECT 
+					sales_invoice_item.description as description_of_goods_services,
+					sales_invoice_item.amount as amount,
+					sales_invoice_item.base_net_amount as taxable_value,
+					sales_invoice_item.item_tax_template as item_tax_template
+				FROM
+    				`tabSales Invoice Item` as `sales_invoice_item`
+				WHERE parent = '{sales_invoice.invoice_name}' {condition_tax_template};
+			""", as_dict = 1)
+
+			# total_taxable_value and total_vat for every single invoice
+			total_taxable_value = 0
+			total_vat = 0
+
+			for item_service in items_services:
+				# get tax rate for each item and calculate VAT
+				tax_rate = frappe.db.get_value('Item Tax Template Detail',
+					{'parent': item_service['item_tax_template']},
+					['tax_rate'])
+				item_service['amount_of_vat'] = item_service['taxable_value'] * 0 if not tax_rate else item_service['taxable_value'] * (tax_rate / 100)
+
+				total_taxable_value += item_service['taxable_value']
+				total_vat += item_service['amount_of_vat']
+				item_service['indent'] = 1
+				report_details.append(item_service)
+
+			sales_invoice['taxable_value'] = total_taxable_value
+			total_taxable_value = 0 #reset total_taxable_value to zero for the next sales invoice
+			sales_invoice['amount_of_vat'] = total_vat
+			total_vat = 0 #reset total_vat to zero for the next sales invoice
+
+			
+
+		report_details = list(filter(lambda report_entry: report_entry['taxable_value'], report_details))
+
+		# separate registered_customer and unregistered customer invoices.
+		registered_customers_sales_invoice_entries = []
+		unregistered_customers_sales_invoice_entries = []
+
+		for report_entry in report_details: 
+			if 'invoice_name' in report_entry:
+				if report_entry['pin_of_purchaser']:
+					registered_customers_sales_invoice_entries += [report_entry]
+				else:
+					unregistered_customers_sales_invoice_entries += [report_entry]	
+
+		registered_customers_total_sales = 0
+		registered_customers_total_vat = 0
+		for entry in registered_customers_sales_invoice_entries:
+			registered_customers_total_sales += entry['invoice_total_sales']
+			registered_customers_total_vat += entry['amount_of_vat']
+
+		unregistered_customers_total_sales = 0
+		unregistered_customers_total_vat = 0
+		for entry in unregistered_customers_sales_invoice_entries:
+			unregistered_customers_total_sales += entry['invoice_total_sales']
+			unregistered_customers_total_vat += entry['amount_of_vat']
+	
+		return report_details
