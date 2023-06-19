6eb38cb0bd3231e173a2d573dc9fd1312bf779e8
Lucky-Tsuma <tsumahmumba@gmail.com>
2023-06-19 13:10:27 +0300

    fix: remove unnecessary column break and section break

--- csf_ke/csf_ke/patches/patch_delete_employee_unnecessary_fields.py
+++ csf_ke/csf_ke/patches/patch_delete_employee_unnecessary_fields.py
@@ -4,3 +4,5 @@ def execute():
     # Delete the custom fields
     frappe.delete_doc("Custom Field", "Employee-column_break_84", force=True)
     frappe.delete_doc("Custom Field", "Employee-section_break_87", force=True)
+    frappe.delete_doc("Custom Field", "Employee-column_break_statutory_details_01", force=True)
+    frappe.delete_doc("Custom Field", "Employee-section_break_statutory_details_01", force=True)
