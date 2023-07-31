717551061ee918f313e09af48810b802e4b99cc5
Lucky-Tsuma <tsumahmumba@gmail.com>
2023-07-31 12:42:39 +0300

    fix: remove `ETR Serial Number` field

--- csf_ke/csf_ke/patches/patch_purchase_invoice.py
+++ csf_ke/csf_ke/patches/patch_purchase_invoice.py
@@ -2,6 +2,11 @@ import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 def execute():
+
+    frappe.delete_doc("Custom Field", "Purchase Invoice-etr_serial_number", force=True)
+    frappe.delete_doc("Custom Field", "Purchase Invoice-etr_column_break", force=True)
+    frappe.delete_doc("Custom Field", "Purchase Invoice-etr_invoice_number", force=True)
+
     custom_fields = {
         "Purchase Invoice": [
             {
@@ -13,28 +18,11 @@ def execute():
                 "translatable": 1
             },
             {
-                "fieldname": "etr_serial_number",
-                "label": "ETR Serial Number",
-                "fieldtype": "Data",
-                "collapsible": 0,
-                "insert_after": "etr_data",
-                "allow_on_submit": True,
-                "translatable": 1
-            },
-            {
-                "fieldname": "etr_column_break",
-                "fieldtype": "Column Break",
-                "collapsible": 0,
-                "insert_after": "etr_serial_number",
-                "allow_on_submit": True,
-                "translatable": 1
-            },
-            {
                 "fieldname": "etr_invoice_number",
                 "label": "ETR Invoice Number",
                 "fieldtype": "Data",
                 "collapsible": 0,
-                "insert_after": "etr_column_break",
+                "insert_after": "etr_data",
                 "allow_on_submit": True,
                 "translatable": 1
             }
