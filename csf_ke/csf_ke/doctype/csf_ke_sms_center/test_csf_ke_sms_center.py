07d44543bc76325e2f33ebb582273a173e59c109
Lucky-Tsuma <55623011+Lucky-Tsuma@users.noreply.github.com>
2023-07-31 13:02:33 +0300

    Revert "added custom doctype: csf_sms_center. Implemented filters on sales in…"

--- csf_ke/csf_ke/doctype/csf_ke_sms_center/test_csf_ke_sms_center.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Navari Limited and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestCSFKESMSCenter(FrappeTestCase):
-	pass
