a7db4f0fac3a6e2ce96474c541a693e9e8ac90ce
Okeyo Gichana Mayaka <gichanamayaka@gmail.com>
2023-10-30 11:32:30 +0300

    added URL validations to ensure the callback URLs are valid links.

--- csf_ke/csf_ke/doctype/mpesa_b2c_settings/mpesa_b2c_settings.js
+++ csf_ke/csf_ke/doctype/mpesa_b2c_settings/mpesa_b2c_settings.js
@@ -1,8 +1,37 @@
 // Copyright (c) 2023, Navari Limited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("MPesa B2C Settings", {
-// 	refresh(frm) {
+frappe.ui.form.on("MPesa B2C Settings", {
+  validate: function (frm) {
+    if (frm.doc.results_url && frm.doc.queue_timeout_url) {
+      let message = "";
+      if (
+        validateURL(frm.doc.results_url) &&
+        validateURL(frm.doc.queue_timeout_url)
+      ) {
+        message = `The Results URL: ${frm.doc.results_url} and Queue TimeOut URL: ${frm.doc.queue_timeout_url} will be used to handle responses from Safaricom.`;
+        frappe.msgprint(message);
+      } else {
+        message = "The URLs Registered are not valid. Please review them";
+        console.log(message);
+        frappe.msgprint(message);
+        frappe.validated = false;
+      }
+    }
+  },
+});
 
-// 	},
-// });
+function validateURL(url) {
+  // validates the input parameter to a valid URL.
+  // url: string, returnType: boolean
+  const pattern = new RegExp(
+    "^((https?|ftp|file):\\/\\/)?" +
+      "((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3}))" +
+      "(\\:\\d+)?(\\/[-a-zA-Z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return pattern.test(url);
+}
