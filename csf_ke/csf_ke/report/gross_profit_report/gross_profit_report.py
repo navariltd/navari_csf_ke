06df92677c95ccbba68d675a78cae10b1de91191
maniamartial <martialmania19@gmail.com>
2023-10-31 21:07:56 +0300

    Calcualte valuation

--- csf_ke/csf_ke/report/gross_profit_report/gross_profit_report.py
+++ csf_ke/csf_ke/report/gross_profit_report/gross_profit_report.py
@@ -3,6 +3,8 @@
 
 # import frappe
 
+from frappe.utils import add_days
+from frappe import qb
 from frappe import get_all, get_value
 from frappe import get_all, qb
 from frappe.utils import flt
@@ -10,6 +12,7 @@ from frappe.utils import flt
 from frappe import _
 import frappe
 from frappe.utils import flt, getdate, comma_and, cstr
+from erpnext.stock.utils import get_incoming_rate
 
 
 # ... (Previous code remains the same)
@@ -33,8 +36,6 @@ def fetch_conversion_factors(data, include_uom):
 
     return conversion_factors
 
-# ... (Rest of the code remains the same)
-
 
 def get_columns(filters):
     columns = [
@@ -50,20 +51,23 @@ def get_columns(filters):
             "fieldname": "item_group",
             "fieldtype": "Link",
             "options": "Item Group",
-            "width": 120
+            "width": 120,
+            "hidden": 1
         },
         {
             "label": "Default UOM",
             "fieldname": "default_uom",
             "fieldtype": "Link",
             "options": "UOM",
-            "width": 120
+            "width": 120,
+            "hidden": 1
         },
         {
             "label": "UOM Required",
             "fieldname": "uom_required",
             "fieldtype": "Data",
-            "width": 100
+            "width": 100,
+            "hidden": 1
         },
         {
             "label": "Qty",
@@ -91,6 +95,7 @@ def get_columns(filters):
             "fieldname": "average_selling_rate",
             "fieldtype": "Currency",
             "width": 120,
+
         },
         {
             "label": "Valuation Rate",
@@ -98,18 +103,21 @@ def get_columns(filters):
             "fieldtype": "Currency",
             "width": 120,
 
+
         },
         {
             "label": "Selling Amount",
             "fieldname": "selling_amount",
             "fieldtype": "Currency",
-            "width": 120
+            "width": 120,
+
         },
         {
             "label": "Buying Amount",
             "fieldname": "buying_amount",
             "fieldtype": "Currency",
             "width": 120,
+
 
         },
 
@@ -119,12 +127,14 @@ def get_columns(filters):
             "fieldtype": "Currency",
             "width": 120,
 
+
         },
         {
             "label": "Gross Profit Percentage",
             "fieldname": "gross_profit_percentage",
             "fieldtype": "Percent",
             "width": 120,
+
 
         },
     ]
@@ -139,14 +149,15 @@ def calculate_gross_profit(buying_amount, selling_amount):
     return selling_amount - buying_amount
 
 
-def calculate_gross_profit_percentage(gross_profit, buying_amount):
+def calculate_gross_profit_percentage(gross_profit, selling_amount):
     gross_profit = float(gross_profit) if gross_profit is not None else 0.0
-    buying_amount = float(buying_amount) if buying_amount is not None else 0.0
+    selling_amount = float(
+        selling_amount) if selling_amount is not None else 0.0
 
-    if buying_amount != 0.0:
-        return (gross_profit / buying_amount) * 100
+    if selling_amount != 0.0:
+        return (gross_profit / selling_amount) * 100
     else:
-        return 100.0  # Avoid division by zero
+        return 0  # Avoid division by zero
 
 
 def get_data(filters):
@@ -160,21 +171,28 @@ def get_data(filters):
     for item in primary_data:
         item_code = item.get('item_code')
 
-        # Calculate the quantity in the chosen UOM
-        chosen_uom = filters.get('item_group')
-        frappe.msgprint(str(chosen_uom))
-        item['qty_uom'] = calculate_qty_in_chosen_uom(
-            item_code, item['qty'], chosen_uom, item['default_uom'])
+        qty = item.get('qty')
 
-        # Recalculate the buying amount using the updated quantity
-        buying_amount = calculate_buying_amount(
-            item_code, filters)
+        # Filters date
+        from_date = filters.get("from_date")
+        to_date = filters.get("to_date")
+
+        # Calculate the quantity in the chosen UOM
+        chosen_uom = filters.get('uom')
+        item['qty_uom'] = calculate_qty_in_chosen_uom(
+            item_code, item['qty'], chosen_uom, item['uom_required'])
+
+        buying_amount = get_buying_amount(item_code, qty, from_date, to_date)
         item['buying_amount'] = buying_amount
 
         selling_amount = item.get('selling_amount', 0)
         item['gross_profit'] = selling_amount - buying_amount
         item['gross_profit_percentage'] = calculate_gross_profit_percentage(
-            item['gross_profit'], buying_amount)
+            item['gross_profit'], selling_amount)
+
+        # Include the valuation rate
+        item['valuation_rate'] = get_valuation_rate_from_sle(
+            item_code, from_date, to_date)
 
         if item_code not in item_dict:
             item_dict[item_code] = item
@@ -182,33 +200,28 @@ def get_data(filters):
             item_dict[item_code]['qty'] += item['qty']
 
         data.append(item)
-        # frappe.msgprint("Rada"+str(data))
 
     return data
 
 
 def calculate_qty_in_chosen_uom(item_code, qty, chosen_uom, sales_invoice_uom):
-    if chosen_uom == sales_invoice_uom:
-        # If the chosen UOM matches the UOM of the sales invoice, return the existing quantity
+    calculated_qty = qty
 
-        return qty
-    else:
-        frappe.msgprint("Nothing")
-        # Use the UOM conversion data to calculate the quantity in the chosen UOM
+    if chosen_uom != sales_invoice_uom:
         conversion_factor = get_conversion_factor(item_code, chosen_uom)
 
         if chosen_uom == "Kg":
-            # If the chosen unit of measure is Kgs, return the existing quantity
-            return qty
-        elif chosen_uom == "Bag":
-            # If the chosen unit of measure is Bags, multiply by 0.04 to get Kgs
-            return qty * 0.04
+            # If the chosen unit of measure is Kgs, calculate the quantity from the existing quantity
+
+            calculated_qty = qty
+        if chosen_uom == "Bag" and sales_invoice_uom == "Kg":
+            calculated_qty = qty / 25
         elif conversion_factor:
+
             # If there is a conversion factor, use it to calculate the quantity
-            return qty * conversion_factor
-        else:
-            # If no conversion factor is found, just return the quantity as it is
-            return qty
+            calculated_qty = qty / conversion_factor
+
+    return calculated_qty
 
 
 def get_conversion_factor(item_code, chosen_uom):
@@ -220,7 +233,7 @@ def get_conversion_factor(item_code, chosen_uom):
     if not conversion_factor:
         conversion_factor = 1.0  # If no conversion factor is found, default to 1
 
-    # frappe.msgprint(str(conversion_factor))
+    # print(str(conversion_factor))
 
     return conversion_factor
 
@@ -273,9 +286,6 @@ def get_query(filters):
     if filters.get("warehouse"):
         conditions.append(f"si_item.warehouse = '{filters.get('warehouse')}'")
 
-    if filters.get("uom"):
-        conditions.append(f"si_item.uom = '{filters.get('uom')}'")
-
     if conditions:
         sql_query += " AND " + " AND ".join(conditions)
 
@@ -285,112 +295,119 @@ def get_query(filters):
     """
     return sql_query
 
-# Calculate buying_amount based on various factors
 
+def get_buying_amount(item_code, qty, from_date, to_date):
+    # Get the valuation rate from the ledger using the item_code
+    valuation_rate = get_valuation_rate_from_sle(item_code, from_date, to_date)
 
-def calculate_buying_amount(item_code, filters):
-    buying_amount = 0.0
-    is_stock_item = frappe.get_value('Item', item_code, 'is_stock_item')
-    last_purchase_rate = 0  # Initialize to 0 by default
-
-    if is_stock_item:
-        last_purchase_rate = get_last_purchase_rate(
-            item_code, filters.get('to_date'))
-
-    if last_purchase_rate:
-        # If a valid last_purchase_rate is available, use this method
-        sle = get_stock_ledger_entries(item_code, filters)
-        for entry in sle:
-            qty = entry.get('actual_qty', 0)  # Handle None as 0
-            buying_amount += qty * last_purchase_rate
-    else:
-        # If last_purchase_rate is not available, use the alternative method
-        sle = get_stock_ledger_entries(item_code, filters)
-        for entry in sle:
-            buying_amount += calculate_buying_amount_from_sle(entry, item_code)
+    # Calculate the buying amount by multiplying valuation rate and quantity
+    buying_amount = valuation_rate * qty
 
     return buying_amount
 
 
-def get_stock_ledger_entries(item_code, filters):
-    sle = frappe.get_all('Stock Ledger Entry', filters={
-        'item_code': item_code,
+# def get_valuation_rate_from_sle(item_code):
+#     sle_entries = frappe.get_all("Stock Ledger Entry",
+#                                  filters={"item_code": item_code},
+#                                  fields=["valuation_rate"],
+#                                  order_by="posting_date DESC",
+#                                  limit_page_length=1)
 
-        'posting_date': ['>=', filters.get('from_date')],
-        'posting_date': ['<=', filters.get('to_date')]
-    }, fields=['actual_qty'])
-
-    return sle
+#     valuation_rate = sle_entries[0].get(
+#         "valuation_rate") if sle_entries else 0.0
+#     return flt(valuation_rate)
 
 
-def get_last_purchase_rate(item_code, to_date):
-    purchase_invoice_item = qb.DocType("Purchase Invoice Item")
-    purchase_invoice = qb.DocType("Purchase Invoice")
+def get_last_purchase_rate(item_code):
+    purchase_invoice = frappe.qb.DocType("Purchase Invoice")
+    purchase_invoice_item = frappe.qb.DocType("Purchase Invoice Item")
 
     query = (
-        qb.from_(purchase_invoice_item)
+        frappe.qb.from_(purchase_invoice_item)
         .inner_join(purchase_invoice)
-        .on(purchase_invoice_item.parent == purchase_invoice.name)
+        .on(purchase_invoice.name == purchase_invoice_item.parent)
         .select(purchase_invoice_item.base_rate / purchase_invoice_item.conversion_factor)
         .where(purchase_invoice.docstatus == 1)
-        .where(purchase_invoice.posting_date <= to_date)
+        # .where(purchase_invoice.posting_date <= self.filters.to_date)
         .where(purchase_invoice_item.item_code == item_code)
-        .orderby(purchase_invoice.posting_date, order=qb.desc)
-        .limit(1)
     )
 
+    query.orderby(purchase_invoice.posting_date, order=frappe.qb.desc)
+    query.limit(1)
     last_purchase_rate = query.run()
-    return flt(last_purchase_rate[0][0]) if last_purchase_rate else 0
+
+    return flt(last_purchase_rate[0][0]) if last_purchase_rate else 0.0
 
 
-def calculate_buying_amount_from_sle(sle, item_code):
-    previous_stock_value = 0.0
-    sle_stock_value = 0.0 if sle.stock_value is None else sle.stock_value
-    sle_qty = 0.0 if sle.qty is None else sle.qty
-    sle_actual_qty = 0.0 if sle.actual_qty is None else sle.actual_qty
+def get_valuation_rate_from_sle(item_code, from_date, to_date):
+    filters = {
+        "item_code": item_code,
+        "docstatus": 1,  # To filter for completed documents
+        "is_cancelled": 0,
+    }
 
-    # Calculate the previous stock value if available
-    previous_sle = get_previous_sle(sle, item_code)
-    if previous_sle:
-        previous_stock_value = previous_sle.get('stock_value', 0.0)
+    if from_date:
+        filters["posting_date"] = (">=", from_date)
 
-    if sle_actual_qty != 0:
-        buying_amount = abs(previous_stock_value -
-                            sle_stock_value) * sle_qty / abs(sle_actual_qty)
+    if to_date:
+        if "posting_date" in filters:
+            filters["posting_date"] = (">=", from_date, "<=", to_date)
+        else:
+            filters["posting_date"] = ("<=", add_days(to_date, 1))
+
+    sle_entries = frappe.get_all("Stock Ledger Entry",
+                                 filters=filters,
+                                 fields=["valuation_rate", "voucher_no", "voucher_type"])
+
+    approved_valuation_rates = []
+
+    for entry in sle_entries:
+        voucher_no = entry.get("voucher_no")
+        voucher_type = entry.get("voucher_type")
+
+        if voucher_no and voucher_type:
+            voucher_status = frappe.get_value(
+                voucher_type, voucher_no, "docstatus")
+            if voucher_status == 1:
+                valuation_rate = flt(entry.get("valuation_rate"))
+                if valuation_rate != 0.0:
+                    approved_valuation_rates.append(valuation_rate)
+
+    if approved_valuation_rates:
+        frappe.msgprint(str(item_code)+":" + str(approved_valuation_rates))
+        average_valuation_rate = sum(
+            approved_valuation_rates) / len(approved_valuation_rates)
+        return flt(average_valuation_rate)
     else:
-        buying_amount = sle_qty * \
-            get_average_buying_rate(item_code, sle.posting_date)
-
-    return buying_amount
+        return 0.0
 
 
-def get_previous_sle(current_sle, item_code):
-    # retrieve the previous entry
-    previous_sle = frappe.get_all("Stock Ledger Entry",
-                                  filters={
-                                      "item_code": item_code,
-                                      "posting_date": ("<", current_sle.get("posting_date")),
-                                      # Adjust this condition based on your needs
-                                      "stock_value": (">", 0)
-                                  },
-                                  fields=["stock_value", "actual_qty"],
-                                  order_by="posting_date desc",
-                                  limit_page_length=1
-                                  )
+# def get_valuation_rate_from_sle(item_code, from_date, to_date):
+#     filters = {
+#         "item_code": item_code,
+#         "docstatus": 1, "is_cancelled": 0,
+#     }
 
-    if previous_sle:
-        return previous_sle[0]
-    else:
-        return {'stock_value': 0.0, 'actual_qty': 0.0}
+#     if from_date:
+#         filters["posting_date"] = (">=", from_date)
 
+#     if to_date:
+#         if "posting_date" in filters:
+#             filters["posting_date"] = (">=", from_date, "<=", to_date)
+#         else:
+#             filters["posting_date"] = ("<=", add_days(to_date, 1))
 
-def get_average_buying_rate(item_code, posting_date):
-    # calculate the average rate
-    average_rate = get_value("Stock Ledger Entry",
-                             filters={
-                                 "item_code": item_code,
-                                 "posting_date": ("<=", posting_date)
-                             },
-                             fieldname=["avg(valuation_rate)"])
+#     sle_entries = frappe.get_all("Stock Ledger Entry",
+#                                  filters=filters,
+#                                  fields=["valuation_rate"])
+#     # frappe.msgprint(str(item_code)+str(sle_entries))
+#     # frappe.msgprint(str(len(sle_entries)))
+#     valuation_rates = [flt(entry.get("valuation_rate"))
+#                        for entry in sle_entries if entry.get("valuation_rate") != 0.0]
 
-    return 0.0 if average_rate is None else average_rate[0][0]
+#     if valuation_rates:
+#         frappe.msgprint(str(sum(valuation_rates)))
+#         average_valuation_rate = sum(valuation_rates) / len(valuation_rates)
+#         return flt(average_valuation_rate)
+#     else:
+#         return 0.0
