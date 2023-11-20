f3760ab8858d73a034624198ac93c3d90d88fa40
maniamartial <martialmania19@gmail.com>
2023-11-20 11:05:52 +0300

    Refactoring sql to qb v-15, helb report

--- csf_ke/csf_ke/report/kenya_helb_report/kenya_helb_report.py
+++ csf_ke/csf_ke/report/kenya_helb_report/kenya_helb_report.py
@@ -6,7 +6,6 @@ import frappe, erpnext
 from frappe import _
 
 def execute(filters=None):
-
 	company_currency = erpnext.get_company_currency(filters.get("company"))
 	columns = get_columns()
 	data = get_data(filters,company_currency)
@@ -43,37 +42,43 @@ def get_columns():
 		
 	return columns
 
-def get_data(filters,company_currency,conditions=""):
-	conditions = get_conditions(filters,company_currency)
+def apply_filters(query, filters, company_currency, salary_slip, salary_detail):
+    doc_status = {"Draft": 0, "Submitted": 1, "Cancelled": 2}
 
-	if filters.from_date > filters.to_date:
-		frappe.throw(_("To Date cannot be before From Date. {}").format(filters.to_date))
+    for filter_key, filter_value in filters.items():
+        if filter_key == "from_date":
+            query = query.where(salary_slip.start_date == filter_value)
+        elif filter_key == "to_date":
+            query = query.where(salary_slip.end_date == filter_value)
+        elif filter_key == "company":
+            query = query.where(salary_slip.company == filter_value)
+        elif filter_key == "salary_component":
+            query = query.where(salary_detail.salary_component == filter_value)
+        elif filter_key == "currency" and filter_value != company_currency:
+            query = query.where(salary_slip.currency == filter_value)
+        elif filter_key == "docstatus":
+            query = query.where(salary_slip.docstatus == doc_status.get(filter_value, 0))
 
-	data = frappe.db.sql("""
-	SELECT  ss.employee, ss.employee_name,
-	        ss.start_date, ss.end_date, 
-			e.national_id, sd.amount
-	FROM `tabEmployee` e, `tabSalary Slip` ss, `tabSalary Detail` sd
-	WHERE %s
-		and e.name = ss.employee
-		and sd.parent = ss.name		
-		and sd.amount != 0	
-	""" % conditions, filters, as_dict=1)
+    return query
 
-	return data
 
-def get_conditions(filters,company_currency):
-	conditions = ""
-	doc_status = {"Draft": 0, "Submitted": 1, "Cancelled": 2}
+def get_data(filters, company_currency):
+    employee = frappe.qb.DocType("Employee")
+    salary_slip = frappe.qb.DocType("Salary Slip")
+    salary_detail = frappe.qb.DocType("Salary Detail")
 
-	if filters.get("docstatus"):
-		conditions += "ss.docstatus = {0}".format(doc_status[filters.get("docstatus")])
+    query = frappe.qb.from_(employee) \
+        .inner_join(salary_slip) \
+        .on(employee.name == salary_slip.employee) \
+        .inner_join(salary_detail) \
+        .on(salary_detail.parent == salary_slip.name) \
+        .select(
+            employee.name,
+            employee.employee_name,
+            employee.national_id,
+            salary_detail.amount
+        ).where(salary_detail.amount != 0)
 
-	if filters.get("from_date"): conditions += " and ss.start_date = %(from_date)s"
-	if filters.get("to_date"): conditions += " and ss.end_date = %(to_date)s"
-	if filters.get("company"): conditions += " and ss.company = %(company)s"
-	if filters.get("salary_component"): conditions += " and sd.salary_component = %(salary_component)s"
-	if filters.get("currency") and filters.get("currency") != company_currency:
-		conditions += " and ss.currency = %(currency)s"
-
-	return conditions
+    query = apply_filters(query, filters, company_currency, salary_slip, salary_detail)
+    data = query.run(as_dict=True)
+    return data
