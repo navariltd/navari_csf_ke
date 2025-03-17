c26115ee04c93aa7e59ded92117338111d5eb74d
emmanuel-mwendwa <mwendwae054@gmail.com>
2025-03-17 11:31:32 +0300

    feat: add dynamic item_code mandatory toggle in Sales Invoice Settings
    - New Single Doctype *Sales Invoice Settings*
    - Used Property Setter to update item_code mandatory status based on set_item_code_mandatory check field

---
+++ csf_ke/csf_ke/doctype/sales_invoice_settings/sales_invoice_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Navari Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sales Invoice Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
