c38c365abe150ee3dde761ff8cebf9b2116d6b84
symonmuchemi <simonkenya846@gmail.com>
2025-05-23 18:41:31 +0300

    feat: Update fixtures for Sales and Purchase Invoices
    Removed outdated fields from the fixtures list and added new fields for Sales and Purchase Invoices to enhance data handling. This includes fields for tracking invoice data, serial numbers, and filing status, ensuring better integration with the ERP system.

--- csf_ke/hooks.py
+++ csf_ke/hooks.py
@@ -28,8 +28,6 @@ fixtures = [
                     "Employee-nhif_no",
                     "Employee-nssf_no",
                     "Employee-tax_id",
-                    "Sales Invoice-etr_invoice_no",
-                    "Purchase Invoice-etr_invoice_no",
                     "Salary Component-p9a_tax_deduction_card_type",
                     "Salary Component-custom_p10a_tax_deduction_card_type",
                     "Item Tax-custom_column_break",
@@ -39,7 +37,21 @@ fixtures = [
                     "Company-custom_withholding_accounts",
                     "Company-custom_default_debitors_withholding_account",
                     "Company-custom_default_creditors_withholding_account",
-                    "Manufacturing Settings-custom_allow_default_time_logs"
+                    "Manufacturing Settings-custom_allow_default_time_logs",
+                    "Sales Invoice-etr_data",
+                    "Sales Invoice-etr_serial_number",
+                    "Sales Invoice-cu_invoice_date",
+                    "Sales Invoice-etr_column_break",
+                    "Sales Invoice-etr_invoice_number",
+                    "Sales Invoice-cu_link",
+                    "Sales Invoice-is_filed",
+                    "Purchase Invoice-etr_data",
+                    "Purchase Invoice-etr_serial_number",
+                    "Purchase Invoice-cu_invoice_date",
+                    "Purchase Invoice-etr_column_break",
+                    "Purchase Invoice-etr_invoice_number",
+                    "Purchase Invoice-cu_link",
+                    "Purchase Invoice-is_filed",
                 ),
             ]
         ],
@@ -175,9 +187,7 @@ doc_events = {
     "Sales Invoice": {
         "before_submit": "csf_ke.csf_ke.overrides.sales_doc.validate_customer_kra"
     },
-    "Job Card":{
-        "before_submit": "csf_ke.csf_ke.overrides.job_card.before_submit"
-    }
+    "Job Card": {"before_submit": "csf_ke.csf_ke.overrides.job_card.before_submit"},
 }
 # Scheduled Tasks
 # ---------------
