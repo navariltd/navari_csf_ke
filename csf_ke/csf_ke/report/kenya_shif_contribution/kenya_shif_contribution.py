4d3ac3a211df4ffdfd244804b3d126fe8b0ff7b8
symonmuchemi <simonkenya846@gmail.com>
2025-07-01 15:41:11 +0300

    feat: implement Kenya SHIF Contributions report generation
    Added functionality to generate the Kenya SHIF Contributions report by defining the execute, get_columns, and get_data functions. The execute function retrieves the report columns and data based on provided filters. The get_columns function defines the structure of the report, including fields such as Payroll Number, First Name, Last Name, Identity Type, Identity Number, KRA PIN, and Contribution Amount. The get_data function constructs and executes a query to fetch relevant data from the Salary Slip, Employee, and Salary Detail DocTypes, filtering for approved salary slips with the SHIF salary component.

--- csf_ke/csf_ke/report/kenya_shif_contribution/kenya_shif_contribution.py
+++ csf_ke/csf_ke/report/kenya_shif_contribution/kenya_shif_contribution.py
@@ -1,9 +1,88 @@
 # Copyright (c) 2025, Navari Ltd and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns = get_columns()
+    data = get_data(filters)
+
+    return columns, data
+
+
+def get_columns():
+    print("Generating columns for Kenya SHIF Contributions report...")
+
+    return [
+        {
+            "fieldname": "payslip_number",
+            "label": "Payroll Number",
+            "fieldtype": "Link",
+            "options": "Salary Slip",
+            "width": 150,
+        },
+        {
+            "fieldname": "first_name",
+            "label": "First Name",
+            "fieldtype": "Data",
+            "width": 150,
+        },
+        {
+            "fieldname": "last_name",
+            "label": "Last Name",
+            "fieldtype": "Data",
+            "width": 150,
+        },
+        {
+            "fieldname": "identity_type",
+            "label": "Identity Type",
+            "fieldtype": "Data",
+            "width": 100,
+        },
+        {
+            "fieldname": "identity_number",
+            "label": "Identity Number",
+            "fieldtype": "Data",
+            "width": 150,
+        },
+        {
+            "fieldname": "tax_id",
+            "label": "KRA PIN",
+            "fieldtype": "Data",
+            "width": 150,
+        },
+        {
+            "fieldname": "contribution",
+            "label": "Contribution Amount",
+            "fieldtype": "Currency",
+            "width": 150,
+        },
+    ]
+
+
+def get_data(filters):
+    SalarySlip = frappe.qb.DocType("Salary Slip")
+    Employee = frappe.qb.DocType("Employee")
+    SalaryDetail = frappe.qb.DocType("Salary Detail")
+
+    query = (
+        frappe.qb.select(
+            SalarySlip.name.as_("payslip_number"),
+            Employee.first_name,
+            Employee.last_name,
+            Employee.tax_id,
+            Employee.national_id.as_("identity_number"),
+            SalaryDetail.amount.as_("contribution"),
+        )
+        .from_(SalarySlip)
+        .join(Employee)
+        .on(SalarySlip.employee == Employee.name)
+        .join(SalaryDetail)
+        .on(SalarySlip.name == SalaryDetail.parent)
+        .where((SalarySlip.docstatus == 1) & (SalaryDetail.salary_component == "SHIF"))
+    )
+
+    data = query.run(as_dict=True)
+
+    return data
