a5c8931c6936cf496b152865b35355c9d4847ac5
symonmuchemi <simonkenya846@gmail.com>
2025-07-01 15:34:33 +0300

    chore: setup report as script type with total row

---
+++ csf_ke/csf_ke/report/kenya_shif_contribution/kenya_shif_contribution.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Navari Ltd and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
