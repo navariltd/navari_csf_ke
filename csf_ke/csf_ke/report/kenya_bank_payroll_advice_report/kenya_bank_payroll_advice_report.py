739465011daed68ec0859294674b612eeca412e1
Emmanuel Mwendwa <mwendwae054@gmail.com>
2025-07-30 11:13:13 +0300

    feat(report): update date filtering logic in get_data and get_conditions functions
    This accommodated fetching data in a range value not strictly equal to i.e,
    - salary_slip_doc.start_date >= filter_value instead of salary_slip_doc.start_date == filter_value
    - salary_slip_doc.end_date >= filter_value instead of salary_slip_doc.end_date == filter_value

--- csf_ke/csf_ke/report/kenya_bank_payroll_advice_report/kenya_bank_payroll_advice_report.py
+++ csf_ke/csf_ke/report/kenya_bank_payroll_advice_report/kenya_bank_payroll_advice_report.py
@@ -61,7 +61,7 @@ def get_columns():
 		
 	return columns
 
-def get_data(filters,company_currency):
+def get_data(filters, company_currency):
 	if filters.from_date > filters.to_date:
 		frappe.throw(_("To Date cannot be before From Date. {}").format(filters.to_date))
   
@@ -85,9 +85,9 @@ def get_conditions(query, filters, company_currency, salary_slip_doc):
     
     for filter_key, filter_value in filters.items():
         if filter_key == "from_date":
-            query = query.where(salary_slip_doc.start_date == filter_value)
+            query = query.where(salary_slip_doc.start_date >= filter_value)
         elif filter_key == "to_date":
-            query = query.where(salary_slip_doc.end_date == filter_value)
+            query = query.where(salary_slip_doc.end_date <= filter_value)
         elif filter_key == "company":
             query = query.where(salary_slip_doc.company == filter_value)
         elif filter_key =="bank_name":
