132409e1cdfa98b01514b45b0df68aab2f78221c
wafula <wafulaat@gmail.com>
2026-07-16 18:04:35 +0300

    chore: apply code formatting

--- csf_ke/csf_ke/report/sales_person_wise_transaction_summary_enhanced/sales_person_wise_transaction_summary_enhanced.py
+++ csf_ke/csf_ke/report/sales_person_wise_transaction_summary_enhanced/sales_person_wise_transaction_summary_enhanced.py
@@ -11,218 +11,217 @@ from frappe.query_builder import Criterion
 
 
 def execute(filters=None):
-    if not filters:
-        filters = {}
+	if not filters:
+		filters = {}
 
-    columns = get_columns(filters)
-    entries = get_entries(filters)
-    item_details = get_item_details()
-    data = []
+	columns = get_columns(filters)
+	entries = get_entries(filters)
+	item_details = get_item_details()
+	data = []
 
-    company_currency = get_company_currency(filters.get("company"))
+	company_currency = get_company_currency(filters.get("company"))
 
-    grouped = OrderedDict()
+	grouped = OrderedDict()
 
-    for d in entries:
-        row = frappe._dict()
-        grouped.setdefault(
-            d.duration,
-            [
-                frappe._dict(
-                    duration=d.duration,
-                    indent=0,
-                    qty=None,
-                    amount=None,
-                    contribution=None,
-                    contribution_qty=None,
-                    contribution_amt=None,
-                )
-            ],
-        ).append(
-            row.update(
-                {
-                    "indent": 1,
-                }
-            )
-        )
+	for d in entries:
+		row = frappe._dict()
+		grouped.setdefault(
+			d.duration,
+			[
+				frappe._dict(
+					duration=d.duration,
+					indent=0,
+					qty=None,
+					amount=None,
+					contribution=None,
+					contribution_qty=None,
+					contribution_amt=None,
+				)
+			],
+		).append(
+			row.update(
+				{
+					"indent": 1,
+				}
+			)
+		)
 
-        if d.stock_qty > 0 or filters.get("show_return_entries", 0):
-            row[frappe.scrub(filters["doc_type"])] = d.name
-            row.customer = d.customer
-            row.territory = d.territory
-            row.warehouse = d.warehouse
-            row.posting_date = d.posting_date
-            row.item_code = d.item_code
-            row.item_group = item_details.get(d.item_code, {}).get("item_group")
-            row.brand = item_details.get(d.item_code, {}).get("brand")
-            row.qty = d.stock_qty
-            row.amount = d.base_net_amount
-            row.sales_person = d.sales_person
-            row.contribution = d.allocated_percentage
-            row.contribution_qty = d.stock_qty * d.allocated_percentage / 100
-            row.contribution_amt = d.contribution_amt
-            row.currency = company_currency
+		if d.stock_qty > 0 or filters.get("show_return_entries", 0):
+			row[frappe.scrub(filters["doc_type"])] = d.name
+			row.customer = d.customer
+			row.territory = d.territory
+			row.warehouse = d.warehouse
+			row.posting_date = d.posting_date
+			row.item_code = d.item_code
+			row.item_group = item_details.get(d.item_code, {}).get("item_group")
+			row.brand = item_details.get(d.item_code, {}).get("brand")
+			row.qty = d.stock_qty
+			row.amount = d.base_net_amount
+			row.sales_person = d.sales_person
+			row.contribution = d.allocated_percentage
+			row.contribution_qty = d.stock_qty * d.allocated_percentage / 100
+			row.contribution_amt = d.contribution_amt
+			row.currency = company_currency
 
-    for group in grouped.values():
-        data.extend(group)
+	for group in grouped.values():
+		data.extend(group)
 
-    return columns, data
+	return columns, data
 
 
 def get_columns(filters):
-    if not filters.get("doc_type"):
-        msgprint(_("Please select the document type first"), raise_exception=True)
+	if not filters.get("doc_type"):
+		msgprint(_("Please select the document type first"), raise_exception=True)
 
-    columns = [
-        {
-            "label": _(filters["duration"][:-2]),
-            "fieldname": "duration",
-            "fieldtype": "Data",
-        },
-        {
-            "label": _(filters["doc_type"]),
-            "options": filters["doc_type"],
-            "fieldname": frappe.scrub(filters["doc_type"]),
-            "fieldtype": "Link",
-            "width": 140,
-        },
-        {
-            "label": _("Customer"),
-            "options": "Customer",
-            "fieldname": "customer",
-            "fieldtype": "Link",
-            "width": 140,
-        },
-        {
-            "label": _("Territory"),
-            "options": "Territory",
-            "fieldname": "territory",
-            "fieldtype": "Link",
-            "width": 140,
-        },
-        {
-            "label": _("Warehouse"),
-            "options": "Warehouse",
-            "fieldname": "warehouse",
-            "fieldtype": "Link",
-            "width": 140,
-        },
-        {
-            "label": _("Posting Date"),
-            "fieldname": "posting_date",
-            "fieldtype": "Date",
-            "width": 140,
-        },
-        {
-            "label": _("Item Code"),
-            "options": "Item",
-            "fieldname": "item_code",
-            "fieldtype": "Link",
-            "width": 140,
-        },
-        {
-            "label": _("Item Group"),
-            "options": "Item Group",
-            "fieldname": "item_group",
-            "fieldtype": "Link",
-            "width": 140,
-        },
-        {
-            "label": _("Brand"),
-            "options": "Brand",
-            "fieldname": "brand",
-            "fieldtype": "Link",
-            "width": 140,
-        },
-        {
-            "label": _("SO Total Qty"),
-            "fieldname": "qty",
-            "fieldtype": "Float",
-            "width": 140,
-        },
-        {
-            "label": _("Amount"),
-            "options": "currency",
-            "fieldname": "amount",
-            "fieldtype": "Currency",
-            "width": 140,
-        },
-        {
-            "label": _("Sales Person"),
-            "options": "Sales Person",
-            "fieldname": "sales_person",
-            "fieldtype": "Link",
-            "width": 140,
-        },
-        {
-            "label": _("Contribution %"),
-            "fieldname": "contribution",
-            "fieldtype": "Float",
-            "width": 140,
-        },
-        {
-            "label": _("Contribution Qty"),
-            "fieldname": "contribution_qty",
-            "fieldtype": "Float",
-            "width": 140,
-        },
-        {
-            "label": _("Contribution Amount"),
-            "options": "currency",
-            "fieldname": "contribution_amt",
-            "fieldtype": "Currency",
-            "width": 140,
-        },
-        {
-            "label": _("Currency"),
-            "options": "Currency",
-            "fieldname": "currency",
-            "fieldtype": "Link",
-            "hidden": 1,
-        },
-    ]
+	columns = [
+		{
+			"label": _(filters["duration"][:-2]),
+			"fieldname": "duration",
+			"fieldtype": "Data",
+		},
+		{
+			"label": _(filters["doc_type"]),
+			"options": filters["doc_type"],
+			"fieldname": frappe.scrub(filters["doc_type"]),
+			"fieldtype": "Link",
+			"width": 140,
+		},
+		{
+			"label": _("Customer"),
+			"options": "Customer",
+			"fieldname": "customer",
+			"fieldtype": "Link",
+			"width": 140,
+		},
+		{
+			"label": _("Territory"),
+			"options": "Territory",
+			"fieldname": "territory",
+			"fieldtype": "Link",
+			"width": 140,
+		},
+		{
+			"label": _("Warehouse"),
+			"options": "Warehouse",
+			"fieldname": "warehouse",
+			"fieldtype": "Link",
+			"width": 140,
+		},
+		{
+			"label": _("Posting Date"),
+			"fieldname": "posting_date",
+			"fieldtype": "Date",
+			"width": 140,
+		},
+		{
+			"label": _("Item Code"),
+			"options": "Item",
+			"fieldname": "item_code",
+			"fieldtype": "Link",
+			"width": 140,
+		},
+		{
+			"label": _("Item Group"),
+			"options": "Item Group",
+			"fieldname": "item_group",
+			"fieldtype": "Link",
+			"width": 140,
+		},
+		{
+			"label": _("Brand"),
+			"options": "Brand",
+			"fieldname": "brand",
+			"fieldtype": "Link",
+			"width": 140,
+		},
+		{
+			"label": _("SO Total Qty"),
+			"fieldname": "qty",
+			"fieldtype": "Float",
+			"width": 140,
+		},
+		{
+			"label": _("Amount"),
+			"options": "currency",
+			"fieldname": "amount",
+			"fieldtype": "Currency",
+			"width": 140,
+		},
+		{
+			"label": _("Sales Person"),
+			"options": "Sales Person",
+			"fieldname": "sales_person",
+			"fieldtype": "Link",
+			"width": 140,
+		},
+		{
+			"label": _("Contribution %"),
+			"fieldname": "contribution",
+			"fieldtype": "Float",
+			"width": 140,
+		},
+		{
+			"label": _("Contribution Qty"),
+			"fieldname": "contribution_qty",
+			"fieldtype": "Float",
+			"width": 140,
+		},
+		{
+			"label": _("Contribution Amount"),
+			"options": "currency",
+			"fieldname": "contribution_amt",
+			"fieldtype": "Currency",
+			"width": 140,
+		},
+		{
+			"label": _("Currency"),
+			"options": "Currency",
+			"fieldname": "currency",
+			"fieldtype": "Link",
+			"hidden": 1,
+		},
+	]
 
-    return columns
+	return columns
 
 
 def get_duration_clause(filters):
-    duration_clause = None
-    if filters["duration"] == "Monthly":
-        duration_clause = "MONTH(dt.{}) as duration"
+	duration_clause = None
+	if filters["duration"] == "Monthly":
+		duration_clause = "MONTH(dt.{}) as duration"
 
-    elif filters["duration"] == "Quarterly":
-        duration_clause = "QUARTER(dt.{}) as duration"
+	elif filters["duration"] == "Quarterly":
+		duration_clause = "QUARTER(dt.{}) as duration"
 
-    elif filters["duration"] == "Yearly":
-        duration_clause = "YEAR(dt.{}) as duration"
+	elif filters["duration"] == "Yearly":
+		duration_clause = "YEAR(dt.{}) as duration"
 
-    elif filters["duration"] == "Weekly":
-        duration_clause = "WEEK(dt.{}) as duration"
+	elif filters["duration"] == "Weekly":
+		duration_clause = "WEEK(dt.{}) as duration"
 
-    if duration_clause:
-        if filters["doc_type"] in ("Sales Invoice", "Delivery Note"):
-            duration_clause = duration_clause.format("posting_date")
+	if duration_clause:
+		if filters["doc_type"] in ("Sales Invoice", "Delivery Note"):
+			duration_clause = duration_clause.format("posting_date")
 
-        else:
-            duration_clause = duration_clause.format("transaction_date")
+		else:
+			duration_clause = duration_clause.format("transaction_date")
 
-    return duration_clause
+	return duration_clause
 
 
 def get_entries(filters):
-    date_field = (
-        filters["doc_type"] == "Sales Order" and "transaction_date"
-    ) or "posting_date"
-    if filters["doc_type"] == "Sales Order":
-        qty_field = "delivered_qty"
-    else:
-        qty_field = "qty"
+	date_field = (filters["doc_type"] == "Sales Order" and "transaction_date") or "posting_date"
 
-    conditions, values = get_conditions(filters, date_field)
-    duration_clause = get_duration_clause(filters)
+	if filters["doc_type"] == "Sales Order":
+		qty_field = "delivered_qty"
+	else:
+		qty_field = "qty"
 
-    entries = frappe.db.sql(
-        """
+	conditions, values = get_conditions(filters, date_field)
+	duration_clause = get_duration_clause(filters)
+
+	entries = frappe.db.sql(
+		"""
         SELECT
             dt.name, dt.customer, dt.territory, dt.{} as posting_date, dt_item.item_code,
             st.sales_person, st.allocated_percentage, dt_item.warehouse,
@@ -245,136 +244,87 @@ def get_entries(filters):
             and dt.docstatus = 1 {}
         ORDER BY st.sales_person, dt.name desc
         """.format(
-            date_field,
-            qty_field,
-            qty_field,
-            qty_field,
-            duration_clause,
-            filters["doc_type"],
-            filters["doc_type"],
-            "%s",
-            conditions,
-        ),
-        tuple([filters["doc_type"], *values]),
-        as_dict=1,
-    )
+			date_field,
+			qty_field,
+			qty_field,
+			qty_field,
+			duration_clause,
+			filters["doc_type"],
+			filters["doc_type"],
+			"%s",
+			conditions,
+		),
+		tuple([filters["doc_type"], *values]),
+		as_dict=1,
+	)
 
-    return entries
+	return entries
 
 
 def get_conditions(filters, date_field):
-    conditions = [""]
-    values = []
+	conditions = [""]
+	values = []
 
-    for field in ["company", "customer", "territory"]:
-        if filters.get(field):
-            conditions.append(f"dt.{field}=%s")
-            values.append(filters[field])
+	for field in ["company", "customer", "territory"]:
+		if filters.get(field):
+			conditions.append(f"dt.{field}=%s")
+			values.append(filters[field])
 
-    if filters.get("sales_person"):
-        lft, rgt = frappe.get_value(
-            "Sales Person", filters.get("sales_person"), ["lft", "rgt"]
-        )
-        conditions.append(
-            f"exists(select name from `tabSales Person` where lft >= {lft} and rgt <= {rgt} and name=st.sales_person)"
-        )
+	if filters.get("sales_person"):
+		lft, rgt = frappe.get_value("Sales Person", filters.get("sales_person"), ["lft", "rgt"])
+		conditions.append(
+			f"exists(select name from `tabSales Person` where lft >= {lft} and rgt <= {rgt} and name=st.sales_person)"
+		)
 
-    if filters.get("from_date"):
-        conditions.append(f"dt.{date_field}>=%s")
-        values.append(filters["from_date"])
+	if filters.get("from_date"):
+		conditions.append(f"dt.{date_field}>=%s")
+		values.append(filters["from_date"])
 
-    if filters.get("to_date"):
-        conditions.append(f"dt.{date_field}<=%s")
-        values.append(filters["to_date"])
+	if filters.get("to_date"):
+		conditions.append(f"dt.{date_field}<=%s")
+		values.append(filters["to_date"])
 
-    items = get_items(filters)
-    if items:
-        placeholders = ", ".join(["%s"] * len(items))
-        conditions.append(f"dt_item.item_code in ({placeholders})")
-        values += items
-    else:
-        # return empty result, if no items are fetched after filtering on 'item group' and 'brand'
-        conditions.append("dt_item.item_code = Null")
+	items = get_items(filters)
+	if items:
+		placeholders = ", ".join(["%s"] * len(items))
+		conditions.append(f"dt_item.item_code in ({placeholders})")
+		values += items
+	else:
+		# return empty result, if no items are fetched after filtering on 'item group' and 'brand'
+		conditions.append("dt_item.item_code = Null")
 
-    return " and ".join(conditions), values
+	return " and ".join(conditions), values
 
 
 def get_items(filters):
-    item = qb.DocType("Item")
+	item = qb.DocType("Item")
 
-    item_query_conditions = []
-    if filters.get("item_group"):
-        # Handle 'Parent' nodes as well.
-        item_group = qb.DocType("Item Group")
-        lft, rgt = frappe.db.get_all(
-            "Item Group",
-            filters={"name": filters.get("item_group")},
-            fields=["lft", "rgt"],
-            as_list=True,
-        )[0]
-        item_group_query = (
-            qb.from_(item_group)
-            .select(item_group.name)
-            .where((item_group.lft >= lft) & (item_group.rgt <= rgt))
-        )
-        item_query_conditions.append(item.item_group.isin(item_group_query))
-    if filters.get("brand"):
-        item_query_conditions.append(item.brand == filters.get("brand"))
+	item_query_conditions = []
+	if filters.get("item_group"):
+		# Handle 'Parent' nodes as well.
+		item_group = qb.DocType("Item Group")
+		lft, rgt = frappe.db.get_all(
+			"Item Group",
+			filters={"name": filters.get("item_group")},
+			fields=["lft", "rgt"],
+			as_list=True,
+		)[0]
+		item_group_query = (
+			qb.from_(item_group)
+			.select(item_group.name)
+			.where((item_group.lft >= lft) & (item_group.rgt <= rgt))
+		)
+		item_query_conditions.append(item.item_group.isin(item_group_query))
+	if filters.get("brand"):
+		item_query_conditions.append(item.brand == filters.get("brand"))
 
-    items = (
-        qb.from_(item)
-        .select(item.name)
-        .where(Criterion.all(item_query_conditions))
-        .run()
-    )
-    return items
+	items = qb.from_(item).select(item.name).where(Criterion.all(item_query_conditions)).run()
+	return items
 
 
 def get_item_details():
-    item_details = {}
-    for d in frappe.db.sql(
-        """SELECT `name`, `item_group`, `brand` FROM `tabItem`""", as_dict=1
-    ):
-        item_details.setdefault(d.name, d)
+	item_details = {}
+	for d in frappe.db.sql("""SELECT `name`, `item_group`, `brand` FROM `tabItem`""", as_dict=1):
+		item_details.setdefault(d.name, d)
 
-    return item_details
-
-
-# fmt = [
-#     "SI-01-2026-00009",
-#     "Brilliance Electric Ltd",
-#     "Kenya",
-#     "Industrial Area Sales - MG",
-#     datetime.date(2026, 1, 6),
-#     "Island Slab Grey Sparkle (2400*1000*20mm)",
-#     "Products",
-#     None,
-#     1.0,
-#     12000.0,
-#     "Sofia Nyambura",
-#     100.0,
-#     1.0,
-#     12000.0,
-#     "KES",
-# ]
-
-
-# data = {
-#     "indent": 1,
-#     "sales_order": "SO-01-2026-00002",
-#     "customer": "Barago construction and suppliers Ltd",
-#     "territory": "Kenya",
-#     "warehouse": "Industrial Area Sales - MG",
-#     "posting_date": datetime.date(2026, 1, 2),
-#     "item_code": "Kenyan Black Granite Slab (2400*600*20mm)",
-#     "item_group": "Products",
-#     "brand": None,
-#     "stock_qty": 4.0,
-#     "base_net_amount": 25862.08,
-#     "sales_person": "Sofia Nyambura",
-#     "allocated_percentage": 100.0,
-#     "contribution": 4.0,
-#     "contribution_amt": 25862.08,
-#     "company_currency": None,
-#     "currency": "KES",
-# }
+	return item_details
